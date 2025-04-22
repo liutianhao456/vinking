5b85194dd877fd3c7904238937eece1e0f632311
User <user@example.com>
2025-04-22 22:28:25 +0800

    fix: simplify configuration for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -3,10 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
-    domains: ['localhost', 'vinking.site', 'www.vinking.site', '76.76.21.241', '66.33.60.130'],
   },
   output: 'standalone',
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://vinking.site' : '',
 }
 
 module.exports = nextConfig 
