fcdd60d08c7ce7739573a947c0bf9384cc816e3a
User <user@example.com>
2025-04-22 22:19:40 +0800

    fix: update Vercel and Next.js configuration

--- next.config.js
+++ next.config.js
@@ -3,14 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
+    domains: ['localhost', 'vinking.site', 'www.vinking.site'],
   },
   output: 'standalone',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://vinking.site' : '',
 }
 
 module.exports = nextConfig 
